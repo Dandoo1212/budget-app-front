589ff9270d8482be4c89aa73aca488cbbea931ab
Damian Michalak <damian.michalak@britenet.com.pl>
2019-06-04 21:00:47 +0200

    Add bootstrap and prime ng to project, create menu bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'budget-app-front';
+export class AppComponent implements OnInit {
+  items: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Check your receipts',
+        icon: 'pi pi-list'
+      },
+      {
+        label: 'Add Receipt',
+        icon: 'pi pi-money-bill'
+      },
+      {
+        label: 'Show diagram',
+        icon: 'pi pi-info'
+      }
+    ];
+  }
+
+
 }
